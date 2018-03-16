02b38b06c4bb15291ce92355f9e64af504d077d9
zeyadetman <zeyadetman@gmail.com>
2018-03-17 00:57:51 +0200

    railfence algorithm added

--- SecurityLibrary/MainAlgorithms/Columnar.cs
+++ SecurityLibrary/MainAlgorithms/Columnar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +16,71 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            List<List<char>> table = new List<List<char>>();
+            Dictionary<int, string> cip = new Dictionary<int, string>();
+            string PT = "";
+            cipherText = cipherText.ToLower();
+            int columns = key.Count;
+            int rows = (int)Math.Ceiling((double)cipherText.Length / columns);
+            for (int i = 0; i < rows; i++)
+            {
+                table.Add(new List<char>());
+            }
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    
+                }
+            }
+
+            return "";
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            int columns = key.Count;
+            int rows = (int)Math.Ceiling((double)plainText.Length / columns);
+            if (plainText.Length != rows * columns)
+            {
+                int x = (rows * columns) - plainText.Length;
+                string appender = new string('x',x);
+                plainText += appender;
+            }
+            List<List<char>> table = new List<List<char>>();
+            Dictionary<int, string> cip = new Dictionary<int, string>();
+            string CT = "";
+            for (int i = 0; i < rows; i++)
+            {
+                table.Add(new List<char>());
+            }
+
+            int counter = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns && counter<plainText.Length; j++)
+                {
+                    table[i].Add(plainText[counter]);
+                    counter++;
+                }
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                string tmp = "";
+                for (int j = 0; j < rows; j++)
+                {
+                    tmp += table[j][i];
+                    cip[key[i] ] = tmp;
+                }
+            }
+
+            for (int i = 1; i <= cip.Count; i++)
+            {
+                CT += cip[i];
+            }
+            Console.WriteLine(CT);
+            return CT.ToUpper();
         }
     }
 }
--- SecurityLibrary/MainAlgorithms/RailFence.cs
+++ SecurityLibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,69 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            List<int> possibleKeys = new List<int>();
+            char sec = cipherText[1];
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                if (plainText[i] == sec) possibleKeys.Add(i);
+            }
+
+            for (int i = 0; i < possibleKeys.Count; i++)
+            {
+                Console.WriteLine(possibleKeys[i].ToString());
+                string s = Encrypt(plainText, possibleKeys[i]).ToLower();
+                Console.WriteLine(cipherText + " " + s);
+                if (String.Equals(cipherText, s))
+                {
+                    Console.WriteLine(possibleKeys[i]);
+                    return possibleKeys[i];
+                }
+            }
+
+            return -1;
+
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            int PTLength = (int) Math.Ceiling((double)cipherText.Length / key);
+            return Encrypt(cipherText, PTLength).ToLower();
+
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            String.Join(plainText,plainText.Split(' '));
+            Console.WriteLine(plainText);
+            List<List<char>> table = new List<List<char>>();
+            int each = (int)Math.Ceiling((double)plainText.Length/key);
+            int counter = 0;
+            string CT = "";
+            for (int i = 0; i < key; i++)
+            {
+                table.Add(new List<char>());
+            }
+
+            for (int i = 0; i < each; i++)
+            {
+                for (int j = 0; j < key && j<plainText.Length; j++)
+                {
+                    table[j].Add(plainText[counter]);
+                    counter++;
+                    if(counter == plainText.Length) break;
+                }
+            }
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                for (int j = 0; j < table[i].Count; j++)
+                {
+                    CT += table[i][j];
+                }
+            }
+            return CT.ToUpper();
         }
     }
 }
