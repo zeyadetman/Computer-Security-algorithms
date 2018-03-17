9483b0a4f9cdf1dd32ca9c741d4d169a40681f51
zeyadetman <zeyadetman@gmail.com>
2018-03-17 20:55:54 +0200

    RepeatingkeyVigenere Algorithm added

--- SecurityLibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ SecurityLibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -10,17 +10,71 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            int clength = cipherText.Length;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string key = "";
+            string temp = "";
+            for (int i = 0; i < clength; i++)
+            {
+                key = key + alphabet[((alphabet.IndexOf(cipherText[i]) - alphabet.IndexOf(plainText[i])) + 26) % 26];
+            }
+            temp = temp + key[0];
+            int klength = key.Length;
+            for (int i = 1; i < klength; i++)
+            {
+                if (cipherText.Equals(Encrypt(plainText, temp)))
+                {
+                    return temp;
+                }
+                temp = temp + key[i];
+            }
+            return key;
+            //throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            int clength = cipherText.Length;
+            string plaintext = "";
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int temp = 0;
+            while (key.Length != clength)
+            {
+                key += key[temp];
+                temp++;
+            }
+            for (int i = 0; i < clength; i++)
+            {
+                plaintext = plaintext + alphabet[((alphabet.IndexOf(cipherText[i]) - alphabet.IndexOf(key[i])) + 26) % 26];
+
+            }
+
+            return plaintext;
+            //throw new NotImplementedException();
+
         }
+
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            int temp = 0;
+            int plength = plainText.Length;
+            string ciphertext = "";
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            while (key.Length != plainText.Length)
+            {
+                key = key + key[temp];
+                temp++;
+            }
+            for (int i = 0; i < plength; i++)
+            {
+                ciphertext = ciphertext + alphabet[((alphabet.IndexOf(plainText[i]) + alphabet.IndexOf(key[i]))) % 26];
+
+            }
+            return ciphertext;
+            //throw new NotImplementedException();
         }
     }
 }
