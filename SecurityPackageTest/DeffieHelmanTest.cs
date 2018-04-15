fcd81518f6db0e14b96918503fdd32b1f4eb7a08
zeyadetman <zeyadetman@gmail.com>
2018-04-15 06:34:27 +0200

    Diffie-Hellman added

---
+++ SecurityPackageTest/DeffieHelmanTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecurityLibrary.DiffieHellman;
+using System.Collections.Generic;
+
+namespace SecurityPackageTest
+{
+    
+    [TestClass]
+    public class DiffieHellmanTest
+    {
+        [TestMethod]
+        public void DeffieHelmanTest1()
+        {
+            DiffieHellman algorithm = new DiffieHellman();
+            List<int> key = algorithm.GetKeys(19, 2, 6, 13);
+            Assert.AreEqual(key[0], 7);
+            Assert.AreEqual(key[1], 7);
+        }
+
+        [TestMethod]
+        public void DeffieHelmanTest2()
+        {
+            DiffieHellman algorithm = new DiffieHellman();
+            List<int> key = algorithm.GetKeys(353, 2, 97, 233);
+            Assert.AreEqual(key[0], 81);
+            Assert.AreEqual(key[1], 81);
+        }
+
+        [TestMethod]
+        public void DeffieHelmanTest3()
+        {
+            DiffieHellman algorithm = new DiffieHellman();
+            List<int> key = algorithm.GetKeys(353, 3, 97, 233);
+            Assert.AreEqual(key[0], 160);
+            Assert.AreEqual(key[1], 160);
+        }
+
+        [TestMethod]
+        public void DeffieHelmanNewTest()
+        {
+            DiffieHellman algorithm = new DiffieHellman();
+            List<int> key = algorithm.GetKeys(541, 10, 50, 100);
+            Assert.AreEqual(key[0], 449);
+            Assert.AreEqual(key[1], 449);
+        }
+    }
+}
---
+++ SecurityPackageTest/ElGamalTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecurityLibrary.ElGamal;
+using System.Collections.Generic;
+
+namespace SecurityPackageTest
+{
+
+    [TestClass]
+    public class ElGamalTest
+    {
+        [TestMethod]
+        public void ElGamalEnc1()
+        {
+            ElGamal algorithm = new ElGamal();
+            List<long> cipher = algorithm.Encrypt(7187, 4842, 4464, 19, 19);//191
+            Assert.AreEqual(cipher[0], 2781);
+            Assert.AreEqual(cipher[1], 437);
+        }
+
+        [TestMethod]
+        public void ElGamalEnc2()
+        {
+            ElGamal algorithm = new ElGamal();
+            List<long> cipher = algorithm.Encrypt(6323, 4736, 2231, 58, 111);//118
+            Assert.AreEqual(cipher[0], 6066);
+            Assert.AreEqual(cipher[1], 899);
+        }
+
+        [TestMethod]
+        public void ElGamalDec1()
+        {
+            ElGamal algorithm = new ElGamal();
+            int plain = algorithm.Decrypt(2781, 437, 191, 7187);
+            Assert.AreEqual(plain, 19);
+        }
+
+        [TestMethod]
+        public void ElGamalDec2()
+        {
+            ElGamal algorithm = new ElGamal();
+            int plain = algorithm.Decrypt(6066, 899, 118, 6323);
+            Assert.AreEqual(plain, 111);
+        }
+    }
+}
+
+
